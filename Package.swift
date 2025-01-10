e71797db83787f57f0810dff7b014de6f0c79ae2
imodeljs-admin <38288322+imodeljs-admin@users.noreply.github.com>
2025-01-10 06:35:29 +0000

    5.0.32

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
   targets: [
     .binaryTarget(
         name: "IModelJsNative",
-        url: "https://github.com/iTwin/mobile-native-ios/releases/download/5.0.31/IModelJsNative.xcframework.zip",
-        checksum: "1a9f95bd3cb6231293975451c2628dfb60bb1c64396c743d0a211ac6df6be62d")
+        url: "https://github.com/iTwin/mobile-native-ios/releases/download/5.0.32/IModelJsNative.xcframework.zip",
+        checksum: "6e35d039193fc03dee125975ba72e5db3d6bf7c3dc1e879879a940b628302629")
   ]
 )
